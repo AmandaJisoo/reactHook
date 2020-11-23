468c96186fcc4fcbba8df4735c649016d878fdfb
AmandaJisoo <amandapark8988@gmail.com>
2020-11-22 19:02:42 -0800

    reducers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 import './App.css';
 
 // line 22, 23 : I pass to the second argument of displatch
+//I don't want to modifu actual state, return new ver of state
 function reducer(state, action) {
   switch(action.type) {
-    case 'INCREMENT':
-        return state + 1
-    case 'DECREMENT':
-      return state -1;
+    case "add-todo":
+        return { 
+          todos: [...state.todos, {text: action.text, completed: false}],
+          todoCount: state.todoCount +1
+        }
+    case "toggle-todo":
+      console.log("nani?");
+      return {
+        todos: state.todos.map((t, idx) => idx === action.idx? {...t, completed: !t.completed} : t),
+        todoCount: state.todoCount
+      }
     default:
       return state;
   }
@@ -15,13 +23,29 @@ function reducer(state, action) {
 
 //only with function
 const App = () => {
-  const [count, dispatch] = useReducer(reducer, 0);
+                                        //reducer is a function and 
+  const [{todos, todoCount}, dispatch] = useReducer(reducer, { todos: [], todoCount: 0 });
+  const [text, setText] = useState();
   return (
     <div>
-      <div>count: {count}</div>
-      <button onClick={() => dispatch( {type: 'INCREMENT' })}>+</button>
-      <button onClick={() => dispatch( {type: 'DECREMENT' })}>-</button>
-    </div>
+        <form onSubmit={evt => {
+          evt.preventDefault();
+          dispatch({type: "add-todo", text});
+          setText("");
+        }}>
+        <input value={text} onChange={evt => setText(evt.target.value)}></input>
+        </form>
+      <div>num of to-dos: {todoCount}</div>
+        {todos.map((t, idx) => (
+          <div key={idx} 
+          onClick={() => dispatch({type: "toggle-todo", idx})}
+          style={{
+            textDecoration: t.completed? 'line-through': ""
+          }}> 
+            {t.text}
+          </div>
+        ))}
+      </div>
   );
 }
 
